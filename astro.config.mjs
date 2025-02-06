ca153bde9e9cc8d783368e09b377411c5042b499
admin@xrayfinance.it <admin@xrayfinance.it>
2025-02-06 18:28:55 +0100

    https problem

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://brix-ia.com", // Deve stare qui, non in experimental
   experimental: {
     contentLayer: true, // Enable content layer collections
   },
